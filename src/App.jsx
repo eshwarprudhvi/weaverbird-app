d10abe86691be43994db34367baad85a46fd9dc9
eshwar <eshwarprudhvi2005@gmail.com>
2026-06-19 18:32:38 +0530

    release: v1.1.4 OTA update bypass cache

--- src/App.jsx
+++ src/App.jsx
@@ -66,7 +66,7 @@ import {
   MoreVertical,
 } from "lucide-react";
 
-const WEB_APP_VERSION = "1.1.3";
+const WEB_APP_VERSION = "1.1.4";
 
 // Default initial data to populate localStorage if empty
 const INITIAL_PROJECTS = []
